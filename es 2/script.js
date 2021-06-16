dec7c475ec87e038bb20b14a8774422c24e02b53
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-16 11:45:21 +0200

    struttura

--- es 2/script.js
+++ es 2/script.js
@@ -1,7 +1,7 @@
 var lato = parseInt(prompt("inserisci la lunghezza di un lato"))
 var geometrica = {
     triangoloEquilatero: [
-        {lato:lato , perimetro: lato * 3, area:(lato * Math.sqrt(3))/2},
+        {lato:lato , perimetro: lato * 3 + "cm", area:(lato * Math.sqrt(3))/2 + "cm^2"},
     ]
 }
 
