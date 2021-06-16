b17a920999fd33f4701e35e27544f23faa168b8e
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-16 11:19:52 +0200

    struttura

---
+++ es 1/script.js
@@ -0,0 +1,8 @@
+var salaGiochi = {
+    giochi: [
+        {name:"crash bandicot" , sale:"70" , age:14},
+    ]
+}
+    var name = prompt("inserisci il nome del gioco")
+    var sale = prompt("inserisci il prezzo del gioco")
+    var name = prompt("inserisci l'età minima del gioco")
