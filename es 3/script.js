0b23f56b0c4e61f11d5a499a6e595be418c52098
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-16 12:46:02 +0200

    struttura

--- es 3/script.js
+++ es 3/script.js
@@ -1,8 +1,35 @@
 var arrayNum = []
 var media = 0
-for (var i = 0; i < 3; i++) {
-    
+for (var i = 0; i < 3; i++) {  
     var num = prompt("inserisci un numero")
     arrayNum.push(num)
     media += arrayNum[i] / 3
 }
+
+var mediaPlayer = 0
+var player = {
+    giocatori: [
+    {
+    nome:"Marco" , 
+    cognome: "Rossi", 
+    anno:"1980",
+    punteggio:10
+    },{
+    nome:"Giovanni" , 
+    cognome: "Sanna", 
+    anno:"1990",
+    punteggio:30
+    },{
+    nome:"Mario" , 
+    cognome: "Porcu", 
+    anno:"1985",
+    punteggio:15
+    }]
+    
+}
+
+for (var i = 0; i < 3; i++) {
+    mediaPlayer += parseInt(player.giocatori[i].punteggio)
+}
+mediaPlayer = mediaPlayer / player.giocatori.length
+console.log(mediaPlayer + " media giocatori" , media+ " media")
