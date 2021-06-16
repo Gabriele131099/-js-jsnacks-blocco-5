a9845daac0736bf06ecc0ad1c0fa459e3bfcc842
gabriele131099 <gabrisanna131099@gmail.com>
2021-06-16 17:02:24 +0200

    struttura

--- es 4/script.js
+++ es 4/script.js
@@ -1,32 +1,39 @@
-var studenti = {
-    studente: [
-        {
-            nome: 'Tony',
-            cognome: "D'Agata",
-            age: 26,
-            descrizione: ''
-          } ,
-          {
-            nome: 'Tommy',
-            cognome: 'Soro',
-            age: 22,
-            descrizione: ''
-          } ,
-          {
-            nome: 'Giovanni',
-            cognome: 'Rossi',
-            age: 23,
-            descrizione: ''
-          } 
-          
-    ]
+var studente = [
+  {
+      nome: "Tony",
+      cognome: "D'Agata",
+      age: 26,
+      descrizione: ""
+    } ,
+    {
+      nome: "Tore",
+      cognome: "Soro",
+      age: 22,
+      descrizione: ''
+    } ,
+    {
+      nome: "Mario",
+      cognome: "Rossi",
+      age: 23,
+      descrizione: ""
+    }   
+]
+
+
+var stampa = document.getElementById('stampa') // stampa.innerHTML = '<li>studente: </li>'
+var variabileRandom = '' //variabile che avrà il contenuto da stampare
+
+for (var i = 0; i < studente.length; i++) { //finchè I è minore della lunghezza di array Studente
+  studente[i].descrizione=(prompt("descrivi lo studente")) //richiede e carica la descrzione degli studenti
+  
+  variabileRandom += '<li>Studente: '    // voglio stampare in un ul , tramide degli LI
+  variabileRandom += studente[i]['nome'] + ', '  // il nome il cognome l'età e la descrizione
+  variabileRandom += studente[i]['cognome'] + ', '   //dello studente I
+  variabileRandom += studente[i]['age'] + ', '
+  variabileRandom += studente[i]['descrizione'] 
+  variabileRandom += '</li>'
 }
 
-for (var i = 0; i < studenti.studente.length; i++) {
+stampa.innerHTML = variabileRandom  //stampo il contenuto di VARIABILERANDOM
 
-   
-        studenti.studente[i].descrizione= prompt("descrivi lo studente")
 
-    
-    
-}
